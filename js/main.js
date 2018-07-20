fbd98bc1d54c89c67f4b03c0f83b6343bb35e2a8
Rendell  Eyas <31457242+tsokoloy@users.noreply.github.com>
2018-07-20 13:35:29 +0800

    Accessing the webcam using html 5 and script

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-$(document).ready(function() {
-    $("#search").val('');
- });
+var video = document.querySelector("#videoElement");
+
+    if (navigator.mediaDevices.getUserMedia) {       
+        navigator.mediaDevices.getUserMedia({video: true})
+        .then(function(stream) {
+            video.srcObject = stream;
+    })
+    .catch(function(err0r) {
+        console.log("Something went wrong!");
+    });
+}
+    // Elements for taking the snapshot
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    var video = document.getElementById("videoElement");
+    // Trigger photo take
+    document.getElementById("snap").addEventListener("click", function() {
+        context.drawImage(video, 0, 0, 500, 400);
+    });
